90de37dc6b681f9ca2a8c622aee53223ee4526d8
KunwarVSuryavanshi <virajkunwar98@gmail.com>
2022-09-13 23:08:30 +0530

    more controls added

--- src/App.js
+++ src/App.js
@@ -3,9 +3,25 @@ import ReactToPDF from "./Component/ReactToPDF";
 
 function App() {
   const refVar = useRef()
+
+  const callbackFn = () => {
+    console.log("Modify function called");
+  }
+
   return (
     <div>
-      <ReactToPDF element={refVar} scale={0.5} fileName='Bazinga'>
+      <ReactToPDF
+        element={refVar}
+        scale={1}
+        cropX={0}
+        cropY={0}
+        removeContainer={true}
+        fileName='Bazinga'
+        logging={true}
+        modifyFn={callbackFn}
+        printStart={[10, 10]}
+        format="PNG"
+      >
         {(toPdf) => (
           <button type="button" onClick={toPdf}>
             Click me
@@ -13,8 +29,12 @@ function App() {
           )
         }
       </ReactToPDF>
-      <div ref={refVar}>
-        Something inside APP.js
+      <div ref={refVar} style={{width: '100vw', height: '100vh', backgroundColor: 'yellow', position: 'relative'}}>
+        Something inside APP.js <br/> <br/>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum consectetur doloribus explicabo excepturi? Doloribus sit laudantium laboriosam quae repudiandae autem nulla! Necessitatibus similique magnam ratione. Amet vitae molestiae autem eveniet.
+        <div style={{bottom: '0px', position: 'absolute'}}>
+          klahsflkahsf
+        </div>
       </div>
     </div>
   );
--- src/Component/ReactToPDF.jsx
+++ src/Component/ReactToPDF.jsx
@@ -6,15 +6,36 @@ function ReactToPDF(props) {
   const convertToPDF = () => {
     if (props.element) {
       html2canvas(props.element.current, {
-        logging: false,
         useCORS: true,
-        scale: props.scale ?? 1
+        scale: props.scale ?? 1,
+        removeContainer: props.removeContainer ?? false,
+        logging: props.logging ?? false,
+        onclone: props.modifyFn,
+        imageTimeout: props.imageTimeout ?? 0,
+        x: props.cropX,
+        y: props.cropY,
+        windowWidth: props.windowWidth,
+        windowHeight: props.windowHeight,
+        width: props.width,
+        height: props.height,
+        backgroundColor: props.backgroundColor ?? null
       })
         .then(resp => {
-          const jsPdf = new jsPDF();
+          const jsPdf = new jsPDF({
+            orientation: 'p',
+            unit: 'mm',
+            format: 'a4'
+          });
           const img = resp.toDataURL();
-          jsPdf.addImage(img, 'JPEG', props.x ?? 10, props.y ?? 10)
+
+          const width = jsPdf.internal.pageSize.getWidth();
+          const height = jsPdf.internal.pageSize.getHeight();
+          console.log('PROPS',props.pdfWidth, width, height)
+          jsPdf.addImage(img, props.format ? props.format === 'png' || props.format === 'PNG' ? 'PNG' : 'WEBP' : 'JPEG', props.printStart?.[0] ?? 0, props.printStart?.[1] ?? 0, 0, 0, props.compression ?? 'NONE')
           jsPdf.save(props.fileName ?? 'PDF_FILE')
+        })
+        .catch((err) => {
+          console.error("Error occurred :/ \n Please check the props being passed are correct or not.\n", err)
         })
     } else {
       throw new Error("No Element Reference Found!! Please provide reference of the element that you want to print.")
